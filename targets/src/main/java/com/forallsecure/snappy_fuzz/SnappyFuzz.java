f4d372108e15112a10aa0cfcd1f7fdcd8877e451
xansec <abrewer@forallsecure.com>
2024-06-21 15:10:29 -0400

    check symmetry

--- targets/src/main/java/com/forallsecure/snappy_fuzz/SnappyFuzz.java
+++ targets/src/main/java/com/forallsecure/snappy_fuzz/SnappyFuzz.java
@@ -14,43 +14,23 @@ public class SnappyFuzz {
 
     public static void fuzzerTestOneInput(FuzzedDataProvider data) {
 	
-        /*
-        String imageFileFP = args[0];
-        Path p = Paths.get(imageFileFP);
-        String imageFile = p.getFileName().toString();
-        String directory = p.getParent().toString();
-
-        int index = imageFile.lastIndexOf('.');
-        String imageFileName = "";
-
-        if (index == -1) {
-            System.out.println("Error => " + imageFile + " has no extension?");
-            System.exit(0);
-        } else {
-            imageFileName = imageFile.substring(0, index);
-        }
-
-        String compressedImageFile = directory + "/" + imageFileName + ".snappy";
-        String decompressedImageFile = directory + "/new-" + imageFile;
-	*/
-
         try {
             byte[] imageData = data.consumeRemainingAsBytes();
             byte[] compressedImageData = Snappy.compress(imageData);
-
-            //FileOutputStream compressedStream = new FileOutputStream(compressedImageFile);
-            //compressedStream.write(compressedImageData);
-            //compressedStream.close();
-
             byte[] decompressedImageData = Snappy.uncompress(compressedImageData);
 
-            // FileOutputStream decompressedStream = new FileOutputStream(decompressedImageFile);
-            // decompressedStream.write(decompressedImageData);
-            // decompressedStream.close();
+            if (imageData.length != decompressedImageData.length) {
+                throw new AssertionError("Decompressed data length does not match original data length");
+            }
+            
+            for (int i = 0; i < imageData.length; i++) {
+                if (imageData[i] != decompressedImageData[i]) {
+                    throw new AssertionError("Decompressed data does not match original data at byte " + i);
+                }
+            }
 
-            // System.out.println(imageFile + " => " + compressedImageFile + " => " + decompressedImageFile);
         } catch (IOException e) {
-            //e.printStackTrace();
+            e.printStackTrace();
         }
     }
 }
